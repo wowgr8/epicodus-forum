04d6d9d203e6f6aaf1ea2c086f6459744a71d0b5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-08 20:48:40 -0700

    add ForumControl tag
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from "react";
 import Header from "./Header";
-import ForumControl from "./ForumControl";
+import ForumControl from "./ForumControl"
+
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <ForumControl />
     </React.Fragment>
     
   );
