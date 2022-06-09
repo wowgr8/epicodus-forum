92250901b11ad8abc6b1d912572d10f0f42ab347
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-08 20:50:07 -0700

    add placeholder for static version
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ReusableForm(){
+  return(
+    <React.Fragment>
+    <p>Reusable Form placeholder</p>
+    <form>
+      <input>
+        <p>input name placeholder</p>
+      </input>
+    </form>
+    </React.Fragment>
+  )
+}
+
+
+export default ReusableForm;
