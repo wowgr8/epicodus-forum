0558dd4432ad4092bebf84fe878a79c088e1b02f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-09 19:45:37 -0700

    add placeholder for static version of site

---
+++ src/components/NewPostForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import { v4 }/ uuid;
+// import ReusableForm;
+
+function NewPostForm(){
+
+  return(
+    <React.Fragment>
+      {/* <ReusableForm       */}
+        <h1> placeholder for submit button: POST!</h1>
+
+      {/* /> */}
+    </React.Fragment>
+  )
+}
+
+export default NewPostForm;
