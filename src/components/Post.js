97b1c1b25df625535ed0dfa4dc84a3021f1f4c87
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-09 19:41:11 -0700

    add placeholder for static version of site

---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Post(){
+  return (
+    <React.Fragment>
+      <div>
+        <h1>UserName:</h1>
+        <h2>Subject:</h2>
+        <h3>Message Post:</h3>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Post;
