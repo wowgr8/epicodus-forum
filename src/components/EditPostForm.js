f9fa3b5177e92374b23dd5fde4bdd3b747c56cd9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-09 19:55:24 -0700

    add placeholder for static version of site

---
+++ src/components/EditPostForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import ReusableForm from "./ReusableForm";
+//import PropTypes from "prop-types";
+
+function EditPostForm () {
+  
+  return (
+    <React.Fragment>
+
+      <h1> ReusablForm element tag placeholder</h1>
+      <p> eventhandler placeholder</p>
+      <p> button text placeholder </p>
+
+    </React.Fragment>
+  );
+}
+
+export default EditPostForm;
