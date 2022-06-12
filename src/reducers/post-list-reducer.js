da1eeb5aedf5bae49dc222f05bc9462b70b8316e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-12 14:40:37 -0700

    updated filename and added reducer for add_post and delete_post actions
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>
    Co-authored-by: Phil Curran <pecurran@hotmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>

---
+++ src/reducers/post-list-reducer.js
@@ -0,0 +1,23 @@
+export default (state = {}, action) => {
+  const { type, username, subject, message, id } = action;
+  switch (type) {
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          username: username,
+          subject: subject,
+          message: message,
+          id: id,
+        }
+      });
+
+    case 'DELETE_POST':
+      let newState =  {...state};
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+  
