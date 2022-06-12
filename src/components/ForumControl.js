7dcc95535ea931b0a0aea9585d8c4d6eb052b0c7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-12 15:20:25 -0700

    add all imports, methods, and conditional - prep for adding redux
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>
    Co-authored-by: Phil Curran <pecurran@hotmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>

--- src/components/ForumControl.js
+++ src/components/ForumControl.js
@@ -1,39 +1,110 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+import PostList from './PostList';
+import PostDetail from './PostDetail';
+import EditPostForm from './EditPostForm';
+import NewPostForm from './NewPostForm';
 
 class ForumControl extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
+      formVisibleOnPage: false,
       selectedPost: null,
       editing: false
     };
   }
 
   // Methods for eventHandlers/buttons go here
+  handleAddingNewPostToForum = (newPost) => {
+    const { dispatch } = this.props;
+    const { username, subject, message, id } = newPost;
+    const action = {
+      type: 'ADD_POST',
+      id: id,
+      username: username,
+      subject: subject,
+      message: message,
+    }
+    dispatch(action);
+    this.setState({formVisibleOnPage: false});
+  }
+
+  handleDeletingPost = (id) => {
+    const { dispatch } = this.props;
+    const action = {
+      type: 'DELETE_POST',
+      id: id
+    }
+    dispatch(action);
+    this.setState({selectedPost: null});
+  }
+
+  handleChangingSelectedPost = (id) => {
+    const selectedPost = this.props.mainPostList[id];
+    this.setState({selectedPost: selectedPost});
+  }
+
+  handleEditingPostInList = (post) => {}
+
+  handleEditClick = () => {
+    console.log("handleEditClick reached!");
+    this.setState({editing: true});
+  }
+
+  handleClick = () => {
+    if (this.state.selectedPost != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedPost: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
 
 
 
   // conditional will go here inside of render method:
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
 
-    // if this, do that
-    // else, do that...
+    if (this.state.editing) {
+      currentlyVisibleState = <EditPostForm post = {this.state.selectedPost} onEditPost = {this.handleEditingPostInList} />
+      buttonText = "Return to Posts";
+  
+    } else if (this.state.selectedPost != null){
+        currentlyVisibleState = <PostDetail 
+          post = {this.state.selectedPost} 
+          onClickingDelete = {this.handleDeletingPost}
+          onClickingEdit = {this.handleEditClick} />
+        buttonText= "Return to Posts";
+  
+      } else if (this.state.formVisibleOnPage) {
+        currentlyVisibleState = <NewPostForm onNewPostCreation={this.handleAddingNewPostToList}/>;
+        buttonText = "Return to Posts";
+  
+      } else {
+        currentlyVisibleState = <PostList postList={this.state.mainPostList} onPostSelection={this.handleChangingSelectedPost}/>;
+        buttonText = "Add Post";
+      };
 
-
-    return(
-      <React.Fragment>
-        <h1>Place holder for what is currently being shown by formVisibleOnPage</h1>
-      </React.Fragment>
-    )
-
+      return (
+        <React.Fragment>
+          {currentlyVisibleState}
+          <button className="btn btn-primary" onClick = {this.handleClick}>{buttonText}</button>
+          <button className="btn btn-secondary" onClick={this.handleClick}>{buttonText}</button>
+        </React.Fragment>
+      );
   }
-
-  // too soon? add after making reducer (& tests), then you can use them in the methods and conditional above. 
-  // ForumControl.propTypes = {
-  //   mainPostList
-  // }
 }
+
+ForumControl = connect()(ForumControl);
 
 export default ForumControl;
