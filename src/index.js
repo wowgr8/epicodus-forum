9aedf58348bd272800e8f3523ea22e83c5b6f3c4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-12 14:41:36 -0700

    add import for reducer
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>
    Co-authored-by: Phil Curran <pecurran@hotmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/forum-reducer'
+import reducer from './reducers/post-list-reducer';
 
 const store = createStore(reducer);
 
