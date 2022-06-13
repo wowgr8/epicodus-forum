b9f9621333adf2391c8eee964866618ff1cddf23
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-12 17:11:46 -0700

    add action.type to switch line.
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>
    Co-authored-by: Phil Curran <pecurran@hotmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>

--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,13 +1,13 @@
 export default (state = {}, action) => {
   const { type, username, subject, message, id } = action;
-  switch (type) {
+  switch (action.type) {
     case 'ADD_POST':
       return Object.assign({}, state, {
         [id]: {
           username: username,
           subject: subject,
           message: message,
-          id: id,
+          id: id
         }
       });
     case 'DELETE_POST':
