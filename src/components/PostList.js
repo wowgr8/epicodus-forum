4bb0e91257111ca60818b30de98c897275f18f27
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-12 17:27:37 -0700

    replaced placeholder with proptypes and loop
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>
    Co-authored-by: Phil Curran <pecurran@hotmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Post from "./Post";
+import PropTypes from "prop-types";
 
-function PostList(){
+const PostList = (props) => {
+  // const { postList, handleChangingSelectedPost } = props;
   return(
     <React.Fragment>
-      <hr/>
-      <p>Place holder for loop that displays all posts in a list.</p>
-      {/* 
-        {mainTicketList.map((ticket, index) =>
-          <Post names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            key={index} />
-        )}
-      */}
-    </React.Fragment>
+    {Object.values(props.postList).map((post, index) =>
+      <Post
+        whenPostClicked = { props.onPostSelection }
+        username={post.username}
+        subject={post.subject}
+        message={post.message}
+        formattedWaitTime={post.formattedWaitTime}
+        id={post.id}
+        key={index}/>
+    )}
+  </React.Fragment>
   );
 }
+
+PostList.propTypes = {
+  postList: PropTypes.object,
+  onPostSelection: PropTypes.func
+};
 
 export default PostList;
